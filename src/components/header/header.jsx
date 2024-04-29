d1b369aa24f3c616da18f255b694a515e722f8f2
NazarMykhailv <mihailiv1011@gmail.com>
2024-04-29 23:36:26 +0300

    Changed header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './header.scss';
-import {NotificationIcon, ProfileIcon} from '../../assets/icons';
+import { NotificationIcon, ProfileIcon } from '../../assets/icons';
 import useAuth from "../../db/user";
 
 const Header = () => {
@@ -13,22 +13,36 @@ const Header = () => {
         }
     }, [user]);
 
-    console.log(userName, 'user')
-    
-    const truncatedUserName = userName.length > 10 ? userName.slice(0, 10) + '...' : userName;
+    const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 768);
+
+    const updateScreenSize = () => {
+        setIsLargeScreen(window.innerWidth >= 768);
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', updateScreenSize);
+
+        return () => {
+            window.removeEventListener('resize', updateScreenSize);
+        };
+    }, []);
+
+    const displayedUserName = isLargeScreen ? userName : (userName.length > 20 ? userName.slice(0, 20) + '...' : userName);
 
     return (
         <header className="header">
             <div className="header__left">
-                <span className="header__greeting">Welcome,</span>
-                <span className="header__userName">{truncatedUserName}</span>
+                <ul className="header__welcome-list">
+                    <li>Welcome,</li>
+                    <li>{displayedUserName}</li>
+                </ul>
             </div>
             <div className="header__right">
                 <div className="header__button">
-                    <NotificationIcon width="24px" height="24px"/>
+                    <NotificationIcon width="24px" height="24px" />
                 </div>
                 <div className="header__button">
-                    <ProfileIcon width="24px" height="24px"/>
+                    <ProfileIcon width="24px" height="24px" />
                 </div>
             </div>
         </header>
