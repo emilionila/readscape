0c588a0854427742d7fe312348ba113afde06de5
Emiliia Kaminska <milja2705@gmail.com>
2024-05-07 21:51:01 +0300

    added auth logic and settings screen fixes

--- src/Root.js
+++ src/Root.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
 import React from "react";
 import {BooksInProgressPage} from "./pages/booksInProgressPage";
 import {SignUpPage} from "./pages/SignUpPage";
@@ -8,19 +8,26 @@ import {WelcomePage} from "./pages/WelcomePage";
 import NewBookPage from './pages/newBookPage/NewBookPage';
 import {NotFound} from "./pages/NotFound";
 import {ProfileSettingsPage} from "./pages/profileSettingsPage";
+import useAuth from "./db/user";
 
 export const Root = () => {
+    const user = useAuth();
+
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<App />} >
-                    <Route index element={<WelcomePage />}/>
-                    <Route path='signup' element={<SignUpPage/>}/>
-                    <Route path='login' element={<LogInPage/>}/>
-                    <Route path='inprogress' element={<BooksInProgressPage/>}/>
-                    <Route path='addbook' element={<NewBookPage/>}/>
-                    <Route path='profile' element={<ProfileSettingsPage/>}/>
-                    <Route path="*" element={<NotFound/>}/>
+                <Route path="/" element={<App/>}>
+                    <Route index element={<WelcomePage/>}/>
+                    <Route path="signup" element={<SignUpPage/>}/>
+                    <Route path="login" element={<LogInPage/>}/>
+                    {user && (
+                        <>
+                            <Route path="inprogress" element={<BooksInProgressPage/>}/>
+                            <Route path="addbook" element={<NewBookPage/>}/>
+                            <Route path="profile" element={<ProfileSettingsPage/>}/>
+                            <Route path="*" element={<NotFound/>}/>
+                        </>
+                    )}
                 </Route>
             </Routes>
         </Router>
